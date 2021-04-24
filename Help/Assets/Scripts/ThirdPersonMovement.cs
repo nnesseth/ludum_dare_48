d7f2ff09e36189e1133eb5bc5893c10db07f3f09
nnesseth <68468669+nnesseth@users.noreply.github.com>
2021-04-24 16:22:05 +1000

    Player Controller Updates and Added House

--- Help/Assets/Scripts/ThirdPersonMovement.cs
+++ Help/Assets/Scripts/ThirdPersonMovement.cs
@@ -8,17 +8,15 @@ public class ThirdPersonMovement : MonoBehaviour
 {
     [SerializeField] private float speed = 6.0f;
     [SerializeField] private float rotateSpeed = 3.0f;
-
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
     void Update()
     {
         CharacterController controller = GetComponent<CharacterController>();
         // Rotate around the y axis
         transform.Rotate(0, Input.GetAxis("Mouse X") * rotateSpeed, 0);
-
-        // Move forward and backward
-        //Vector3 forward = transform.TransformDirection(Vector3.forward);
-        //float currentSpeed = speed * Input.GetAxis("Vertical");
-        //controller.SimpleMove(forward * currentSpeed);
         
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         controller.transform.Translate(move * Time.deltaTime * speed);
